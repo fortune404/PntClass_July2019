f3cd81ec6b357e0493fe801f0611b3c14df90bb6
fortune404 <rk815.rahi@gmail.com>
2019-09-15 20:41:31 -0700

    class

--- src/seleniumFirstClass/HomePage.java
+++ src/seleniumFirstClass/HomePage.java
@@ -7,16 +7,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HomePage
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.gecko.driver", "/Users/fortunecookie/IdeaProjects/PntClass_July2019/browser-driver/geckodriver");
         WebDriver driver = new FirefoxDriver();
 
         driver.get("https://www.amazon.com/");
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("USBC CABLE");
-        driver.findElement(By.cssSelector(".nav-input"));
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("USBC CABLE");
 
 
+        Thread.sleep(2000);
         driver.close();
 
     }
